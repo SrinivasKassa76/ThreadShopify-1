a97092cb7fcc6007ba8d79f0a62a552cc8d535e9
AnusmitaKundu <AnusmitaKundu@PSDLAP361>
2024-10-30 13:51:24 +0530

    Open browser added

---
+++ Object Repository/Open Browser Object/Page_Threads/div_Enter using password.rs
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Enter using password</name>
+   <tag></tag>
+   <elementGuidId>76ea402a-c16e-4385-8937-d69ca6911fb0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>div.modal__toggle-open.password-link.link.underlined-link</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='shopify-section-main-password-header']/div/div/password-modal/details/summary/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:role=button[name=&quot;Enter using password&quot;i]</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>6272fb04-cd03-4084-82a0-ab5c7486bcae</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>modal__toggle-open password-link link underlined-link</value>
+      <webElementGuid>7cf07ad6-e7a2-4624-b3c3-0346fa6f5c19</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+            
+
+            Enter using password
+          </value>
+      <webElementGuid>38dfdc22-e8eb-41a1-9e3c-e49e77708870</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;shopify-section-main-password-header&quot;)/div[@class=&quot;color-scheme-1 gradient&quot;]/div[@class=&quot;password-header&quot;]/password-modal[1]/details[@class=&quot;password-modal modal&quot;]/summary[@class=&quot;modal__toggle&quot;]/div[@class=&quot;modal__toggle-open password-link link underlined-link&quot;]</value>
+      <webElementGuid>554bae8f-f1a0-41cd-8693-5d5931bda7f0</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='shopify-section-main-password-header']/div/div/password-modal/details/summary/div</value>
+      <webElementGuid>d5391d84-3dad-4404-9ab7-8844221b7d46</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skip to content'])[1]/following::div[4]</value>
+      <webElementGuid>35b8521c-fc87-4352-b7f7-0420c9cf9358</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Enter store using password:'])[1]/preceding::div[2]</value>
+      <webElementGuid>652344d1-edff-41bd-800e-efbee99bd4df</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Your password'])[1]/preceding::div[2]</value>
+      <webElementGuid>9e1e2e2e-97b0-4706-ba79-ab01f0c72af4</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Enter using password']/parent::*</value>
+      <webElementGuid>652ee7a7-27b0-4fac-ace8-510eaa0a17a8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//summary/div</value>
+      <webElementGuid>7ff4ce5b-4b2a-402e-83ad-76889611b3f8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '
+            
+
+            Enter using password
+          ' or . = '
+            
+
+            Enter using password
+          ')]</value>
+      <webElementGuid>a86a8362-c086-4b0f-b324-d4c2d93d14d1</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
